8eb2686ebf2e9a46fe054642de6d0d9da099bdee
gear5km <github.com/gear5km>
2020-05-19 10:13:59 +0300

    v0.3
    More effecient pizzaObject pricing functionality

--- script.js
+++ script.js
@@ -6,20 +6,22 @@
 
 
     //Creates Variables to Store pizzaObject attributes
-    var flavourSelect;
-    var sizeSelect;
-    var crustSelect;
+
+    var flavourSelect=[1];
+    var sizeSelect=[1];
+    var crustSelect=[1];
     var pizza;
+
     function flavour1(){
-      flavourSelect="flavour1";
+      flavourSelect=["flavour1", 100];
     }
 
     function size1(){
-      sizeSelect="size1";
+      sizeSelect=["size1",100];
     }
 
     function crust1(){
-      crustSelect="crust1";
+      crustSelect=["crust1",100];
     }
    
   //pizzaObject constructor
@@ -28,13 +30,14 @@
           this.flavour=flavour;
           this.size=size;
           this.crust=crust;
-          this.price=[];
+          this.price=flavour[1];
       }
 
       
       function comp(){
         alert("working pizzaObj Button")
-        pizza = new pizzaObject("fla","siz","cru");
+        pizza = new pizzaObject(flavourSelect,sizeSelect,crustSelect);
+        pricing();
 
       }
 
@@ -57,17 +60,17 @@ $(document).ready(function(){
       //YOU CANNOT CALL JQuery FUNCTIONS FROM OUTSIDE AND EXPECT A RETURN VALUE!
       //flavourSelect="flavour1";
       flavour1();
-      alert(flavourSelect);
+      //alert(flavourSelect);
     })
 
     $('button#size-button-1').click(function(){
       size1();
-      alert(sizeSelect);
+      //alert(sizeSelect);
     })
 
     $('button#crust-button-1').click(function(){
       crust1();
-      alert(crustSelect);
+      //alert(crustSelect);
     })
 
     $('button#object-button-1').click(function comp(){})
@@ -79,32 +82,3 @@ $(document).ready(function(){
 
 })
 
-
-
-
-
-
-
-//var chosen=new pizzaObject(flavourSelect,sizeSelect,crustSelect);
-
-
-
-  /****************
-  *BUSINESS LOGIC
-  *
-  *****************/
-  /*
-  var pizzaPrice= function price(){
-    flavour1.price=100;
-    size1.price=100;
-    crust1.price=100;
-
-    var totalPrice=flavour1+size1+crust1;
-    return totalPrice;
-  }
-
-  var totalPrice=pizzaPrice();
-  alert(totalPrice);
-
-*/
-  
